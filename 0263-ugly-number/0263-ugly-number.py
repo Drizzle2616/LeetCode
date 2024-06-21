11d2a6ec80d758a2ec68ab1c4fc220b2f4fab9ad
Drizzle2616 <163808944+Drizzle2616@users.noreply.github.com>
2024-06-21 15:25:41 +0900

    Time: 41 ms (25.77%), Space: 16.5 MB (92.31%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0: 
+            return False 
+        primes = [2, 3, 5]
+        for i in range(len(primes)): 
+            while n % primes[i] == float(0): 
+                n = n / primes[i]
+        return n == float(1)
